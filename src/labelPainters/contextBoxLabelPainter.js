2ff1dd59054d391d4cd74d0cf8d5c65a64563931
Will <me@willcannings.com>
2010-07-14 17:14:22 +1000

    Now drawing the context box label as a single path

--- src/labelPainters/contextBoxLabelPainter.js
+++ src/labelPainters/contextBoxLabelPainter.js
@@ -5,20 +5,12 @@ var ContextBoxLabelPainter = function(options) {
 ContextBoxLabelPainter.defaults = {
   xOffset: 0,
   yOffset: 0,
-  box: {
-    width: 85,
-    height: 40,
-    style: {
-      fill: '#000',
-      stroke: '#000'
-    }
-  },
-  contextArrow: {
-    length: 12,
-    style: {
-      fill: '#000',
-      stroke: '#000'
-    }
+  width: 85,
+  height: 40,
+  contextArrowLength: 12,
+  style: {
+    fill: '#000',
+    stroke: '#000'
   },
   label1: {
     top: 12,
@@ -47,22 +39,25 @@ ContextBoxLabelPainter.defaults = {
 ContextBoxLabelPainter.prototype = {
   drawLabel: function(paper, point) {
     point.labelShape = paper.set();
-    
-    // TODO: the box and arrow should be draw as a single path
-    // main label rectangle
     var symbolY = point.symbolShape.topY;
     var boxX = point.x + this.xOffset - this.halfBoxWidth();
-    var boxY = symbolY + this.yOffset - this.box.height - this.contextArrowHeight();
-    point.labelShape.push(paper.rect(boxX, boxY, this.box.width, this.box.height).attr(this.box.style));
+    var boxY = symbolY + this.yOffset - this.height - this.contextArrowHeight();
+    
+    // left and bottom sides of the box
+    var path = ['M', boxX, boxY, 'L', boxX, boxY + this.height];
     
     // context arrow
-    var p1x = point.x - this.halfContextArrowLength();
-    var p1y = symbolY + this.yOffset - this.contextArrowHeight();
-    var p2x = p1x + this.contextArrow.length;
-    var p2y = p1y;
-    var p3x = p1x + this.halfContextArrowLength();
-    var p3y = p1y + this.contextArrowHeight();
-    point.labelShape.push(paper.path(['M', p1x, p1y, 'L', p2x, p2y, 'L', p3x, p3y].join(',')).attr(this.contextArrow.style));
+    var p1x = boxX + this.halfBoxWidth() - this.halfContextArrowLength();
+    var p1y = boxY + this.height;
+    var p2x = p1x + this.halfContextArrowLength();
+    var p2y = p1y + this.contextArrowHeight();
+    var p3x = p1x + this.contextArrowLength;
+    var p3y = p1y;
+    path.push('L', p1x, p1y, 'L', p2x, p2y, 'L', p3x, p3y);
+
+    // bottom, right and top sides of the box
+    path.push('L', boxX + this.width, boxY + this.height, 'L', boxX + this.width, boxY, 'Z');    
+    point.labelShape.push(paper.path(path.join(',')).attr(this.style));
     
     // label text
     point.labelShape.push(paper.text(boxX + this.halfBoxWidth() + this.label1.left, boxY + this.label1.top, point.label1).attr(this.label1.style));
@@ -74,10 +69,12 @@ ContextBoxLabelPainter.prototype = {
       
       point.symbolShape.node.onmouseover = function() {
         point.labelShape.animate(animation.to, animation.duration, animation.easing);
+        paper.safari();
       }
       
       point.symbolShape.node.onmouseout = function() {
         point.labelShape.animate(animation.from, animation.duration, animation.easing);
+        paper.safari();
       }
     }
   },
@@ -101,8 +98,8 @@ ContextBoxLabelPainter.prototype = {
   },
   
   _cacheContextArrowDimensions: function() {
-    this._contextArrowHeight = Math.ceil(Math.sqrt(Math.pow(this.contextArrow.length, 2) / 2));
-    this._halfContextArrowLength = Math.ceil(this.contextArrow.length / 2);
-    this._halfBoxWidth = Math.ceil(this.box.width / 2);
+    this._contextArrowHeight = Math.ceil(Math.sqrt(Math.pow(this.contextArrowLength, 2) / 2));
+    this._halfContextArrowLength = Math.ceil(this.contextArrowLength / 2);
+    this._halfBoxWidth = Math.ceil(this.width / 2);
   }
 }
